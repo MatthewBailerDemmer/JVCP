164c8003a189f0251c6ba581fa6969b9629a8f2d
MatthewBailerDemmer <matheusbailerdemmer@gmail.com>
2023-02-13 20:08:07 -0300

    Estudando formas geométricas em java

---
+++ src/chapter13/Metrics.java
@@ -0,0 +1,13 @@
+package chapter13;
+import javax.swing.JFrame;
+public class Metrics {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Demonstrating FontMetrics");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MetricsJPanel metricsJPanel = new MetricsJPanel();
+        frame.add(metricsJPanel);
+        frame.setSize(510, 240);
+        frame.setVisible(true);
+    }
+}
